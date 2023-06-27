989ae34e98a9dce501cfc3f0cc75bf2265110052
Osman Yusuf Akkus <osmanyusufakkus@gmail.com>
2023-06-27 17:25:06 +0300

    Add new content

--- news-homepage/src/components/MostViewedContainer.tsx
+++ news-homepage/src/components/MostViewedContainer.tsx
@@ -10,10 +10,9 @@ export default function MostViewedContainer({}: Props) {
         <MostViewedNewItem imagename='/image-retro-pcs.jpg'
         newsNumber='01' header='Reviving Retro PCs' brief='What happens when old PCs are given modern upgrades?'/>
         <MostViewedNewItem imagename='/image-top-laptops.jpg'
-        newsNumber='01' header='Reviving Retro PCs' brief='What happens when old PCs are given modern upgrades?'/>
+        newsNumber='02' header='Top 10 Laptops of 2022' brief='Our best picks for various needs and budgets.'/>
         <MostViewedNewItem imagename='/image-gaming-growth.jpg'
-        newsNumber='01' header='Reviving Retro PCs' brief='What happens when old PCs are given modern upgrades?'/>
-        
+        newsNumber='03' header='The Growth of Gaming' brief='How the pandemic has sparked fresh opportunities.'/>
     </div>
   )
 }
