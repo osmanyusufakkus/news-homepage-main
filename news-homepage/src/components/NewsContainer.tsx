3b616de30a22d6880943561b7fc2a5bd77581915
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-06-27 15:32:02 +0300

    Add hr as news splitter

--- news-homepage/src/components/NewsContainer.tsx
+++ news-homepage/src/components/NewsContainer.tsx
@@ -3,10 +3,12 @@ import News from './News'
 
 export default function NewsContainer() {
   return (
-    <div className='col-span-12 md:col-span-4 w-100 h-96 bg-slate-950 p-4'>
-      <span className='text-3xl text-primary-softOrange font-semibold mb-4'>New</span>
+    <div className='col-span-12 md:col-span-4 w-100 bg-slate-950 p-6'>
+      <span className='text-3xl text-primary-softOrange font-semibold my-4'>New</span>
       <News title="Hydrogen vs Electric Cars" content='Will hydrogen-fueled cars ever catch up to EVs?'/>
+      <hr className='bg-gray-500 border-none h-[2px] my-6' />
       <News title="The Downsides of AI Artistry" content='What are the possible adverse effect of on-demand AI image generation?'/>
+      <hr className='bg-gray-500 border-none h-[2px] my-6' />
       <News title="Is VC Funding Drying Up?" content='Private funding by VC firms is down 50% YOY. We take a look at what that means.'/>
     </div>
   )
