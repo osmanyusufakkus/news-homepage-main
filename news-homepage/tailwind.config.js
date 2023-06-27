619e03015beb1e26d638a47bb7a454277aff447e
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-06-27 21:58:01 +0300

    Define mb and md

--- news-homepage/tailwind.config.js
+++ news-homepage/tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      'mb': '540px',
+      'md': '768px',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
