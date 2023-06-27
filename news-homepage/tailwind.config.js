c96f22fe28798bf6fce40363bbca916fbaf308e5
Osman Yusuf Akkus <osmanyusufakkus@gmail.com>
2023-06-27 17:22:32 +0300

    Add new colors

--- news-homepage/tailwind.config.js
+++ news-homepage/tailwind.config.js
@@ -17,6 +17,12 @@ module.exports = {
           softOrange: 'hsl(35, 77%, 62%)',
           softRed: 'hsl(5, 85%, 63%)',
         },
+        neutral:{
+          offWhite: 'hsl(36, 100%, 99%)',
+          grayishBlue: 'hsl(233, 8%, 79%)',
+          darkGrayishBlue: 'hsl(236, 13%, 42%)',
+          veryDarkBlue: 'hsl(240, 100%, 5%)',
+        },
       },
     },
   },
