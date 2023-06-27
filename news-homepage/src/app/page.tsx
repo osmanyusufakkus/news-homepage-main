793ca1c1d3446011255a998bcca896539b554db8
Osman Yusuf Akkus <osmanyusufakkus@gmail.com>
2023-06-27 17:24:11 +0300

    Arrenge new component

--- news-homepage/src/app/page.tsx
+++ news-homepage/src/app/page.tsx
@@ -1,13 +1,14 @@
 import MostViewedContainer from "@/components/MostViewedContainer";
 import MostViewedNews from "@/components/MostViewedNews";
 import NewsContainer from "@/components/NewsContainer";
+import WebHeader from "@/components/WebHeader";
 
 
 export default function Home() {
   return (
     <main className="mx-auto p-4 w-[1440px]">
       
-      <div className='w-full h-20 bg-slate-400'></div>
+      <WebHeader/>
       <div className='grid grid-cols-12 gap-4 my-4'>
         <div className='col-span-12 md:col-span-8 w-100 h-[420px] bg-slate-500'></div>
         <NewsContainer/>
