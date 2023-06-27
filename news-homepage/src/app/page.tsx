3ebafeb0b68e51d9d7cbbb8f85b762b317e19039
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-06-27 15:32:22 +0300

    Add height to the placeholder

--- news-homepage/src/app/page.tsx
+++ news-homepage/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       
       <div className='w-full h-20 bg-slate-400'></div>
       <div className='grid grid-cols-12 gap-4 my-4'>
-        <div className='col-span-12 md:col-span-8 w-100 h-96 bg-slate-500'></div>
+        <div className='col-span-12 md:col-span-8 w-100 h-[420px] bg-slate-500'></div>
         <NewsContainer/>
       </div>
         <MostViewedContainer/>
