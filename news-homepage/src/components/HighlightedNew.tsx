b3e99fd2aeeadafbdb198e31c920406bd102f6a3
Osman Yusuf Akkus <osmanyusufakkus@gmail.com>
2023-06-29 01:32:01 +0300

    Arrange component

--- news-homepage/src/components/HighlightedNew.tsx
+++ news-homepage/src/components/HighlightedNew.tsx
@@ -4,15 +4,18 @@ type Props = {}
 
 export default function HighlightedNew({}: Props) {
   return (
-    <div className='col-span-12 md:col-span-8 w-100 h-[420px] bg-slate-500'>
+    <div className='col-span-12 md:col-span-8 w-100'>
         <div>
-            <img className='object-contain' src='/image-web-3-desktop.jpg'/>
+            <img className='object-contain hidden mb:block' src='/image-web-3-desktop.jpg'/>
+            <img className='object-contain block mb:hidden' src='/image-web-3-mobile.jpg'/>
         </div>
-            <div className='flex flex-row grid grid-cols-2 mb:col-span-2'>
-                <span className=''>The Bright Future of Web 3.0?</span>
-                <span>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</span>
+        <div className='grid grid-cols-2 gap-4 p-4'>
+            <span className='text-[55px] font-bold col-span-2 mb:col-span-1 leading-[55px]'>The Bright Future of Web 3.0?</span>
+            <div className='flex flex-col gap-8 col-span-2 mb:col-span-1'>
+                <span className='text-neutral-darkGrayishBlue '>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</span>     
+                <button className="bg-primary-softRed text-white font-normal text-sm tracking-[4px] py-3 px-4 w-[160px]">READ MORE</button>
             </div>
-    </div>
-    
+        </div>
+    </div> 
   )
 }
